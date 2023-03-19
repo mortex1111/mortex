6976d1cd1f4741b1a5893c36ccc60d6091de87f0
mortex1111 <128226208+mortex1111@users.noreply.github.com>
2023-03-19 01:04:22 +0100

    asd

--- boxtest/Assets/code/playersc.cs
+++ boxtest/Assets/code/playersc.cs
@@ -2,11 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class playersc : MonoBehaviour
 {
     public float speed = 5f; // speed of movement
-    public float jumpForce = 5f; // force of the jump
+    public float jumpForce = 25f; // force of the jump
     private Rigidbody2D rb;
     private bool isGrounded = false;
 
@@ -15,7 +14,6 @@ public class playersc : MonoBehaviour
         rb = GetComponent<Rigidbody2D>(); // get the Rigidbody2D component
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
         float horizontalInput = Input.GetAxis("Horizontal"); // get horizontal input axis value
@@ -27,16 +25,18 @@ public class playersc : MonoBehaviour
         // add vertical force to player when jump button is pressed and player is grounded
         if (jumpInput && isGrounded == true)
         {
-            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
     }
-   private void OnCollisionEnter2D(Collision2D collision)
+
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
             isGrounded = true;
         }
     }
+
     private void OnCollisionExit2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
