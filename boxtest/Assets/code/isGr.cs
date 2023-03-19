b86cf2635e3cf0735df48061a70719a9c75398fa
mortex1111 <128226208+mortex1111@users.noreply.github.com>
2023-03-19 12:22:40 +0100

    asd

--- boxtest/Assets/code/isGr.cs
+++ boxtest/Assets/code/isGr.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class isGr : MonoBehaviour
 {
+    public bool isGrounded;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +15,21 @@ public class isGr : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
     }
 }
